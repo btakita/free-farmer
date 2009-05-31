b96a1a318f4c99be3a8cc303290b433b4008a0b3
Brian Takita <brian.takita@gmail.com>
2009-05-31 14:42:26 -0700

    Revert "Using rspec and RR for testing."
    This reverts commit 00bec6d28d61828183016dc5412e0123415d1454.

--- spec/main.rb
+++ spec/main.rb
@@ -1,13 +1,19 @@
-require File.expand_path("#{File.dirname(__FILE__)}/spec_helper")
+require 'ramaze'
+require 'ramaze/spec'
+
+require __DIR__('../start')
+Ramaze.options.roots = __DIR__('../')
 
 describe MainController do
-  it 'shows start page' do
+  behaves_like :mock
+
+  should 'show start page' do
     get('/').status.should == 200
     last_response['Content-Type'].should == 'text/html'
     last_response.should =~ /<h1>Welcome to Ramaze!<\/h1>/
   end
 
-  it 'shows /notemplate' do
+  should 'show /notemplate' do
     get('/notemplate').status.should == 200
     last_response['Content-Type'].should == 'text/html'
     last_response.should =~ /there is no 'notemplate\.xhtml' associated with this action/
